3bfc0ca10c8a61c25c56773855692ea0184fbc14
Alessandra Carnevali <alessandracarnevali@AirdiAlessandra.homenet.telecomitalia.it>
2025-02-26 07:38:01 -0800

    Traccia 2 completa

---
+++ Esercitazione1/traccia2.php
@@ -0,0 +1,21 @@
+
+
+<?php
+
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perché";
+$text6 = "hai";
+$text7 = "bevuto";
+$text8 = "tutto";
+$text9 =".";
+
+echo $text1 ." ". $text2 ." ". $text3 . $text4." ". $text5 ." ". $text6 ." ". $text7 ." ". $text8 . $text9;
+
+
+
+?>
+
+
