13ca2cc5ab2f67283ba3d6a18512d635cdcbefab
Alessandra Carnevali <alessandracarnevali@AirdiAlessandra.homenet.telecomitalia.it>
2025-02-26 07:09:12 -0800

    Traccia 1 completa

---
+++ Esercitazione1/test.php
@@ -0,0 +1,48 @@
+
+
+ //==================================TRACCIA1==============================
+
+
+<?php
+
+$numero_intero = 15;
+$numero_decimale = 35.2;
+$stringa = "Ciao a tutti";
+$vieni_alla_festa = True;
+
+
+var_dump($numero_intero);
+var_dump($numero_decimale);
+var_dump($stringa);
+var_dump($vieni_alla_festa);
+
+const NUMERO_INTERO = 15;
+const NUMERO_DECIMALE = 35.2;
+const STRINGA = "Ciao a tutti";
+const VIENI_ALLA_FESTA = True;
+
+
+var_dump(NUMERO_INTERO);
+var_dump(NUMERO_DECIMALE);
+var_dump(STRINGA);
+var_dump(VIENI_ALLA_FESTA);
+
+
+
+
+//==================================TRACCIA 2====================================
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
