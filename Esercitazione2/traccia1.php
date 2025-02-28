dfae550108a3f8b8603cdd4f4273b5b241812a02
Alessandra Carnevali <alessandracarnevali@AirdiAlessandra.homenet.telecomitalia.it>
2025-02-28 07:45:15 -0800

    Esercizio uno completo

--- Esercitazione2/traccia1.php
+++ Esercitazione2/traccia1.php
@@ -1,6 +1,31 @@
 <?php
 
+//=================================TRACCIA1=========================
 
+
+$users =[
+
+    ["name" => "Davide", "Cognome" => "Cariola", "Genere" => "Uomo"],
+    ["name" => "Maria", "Cognome" => "Pascoli", "Genere" => "Donna"],
+    ["name" => "Luca", "Cognome" => "Ariota", "Genere" => "NB"]
+
+];
+
+foreach($users as $user){
+    if($user["Genere"] == "Donna"){
+        echo "Buongiorno Signora {$user["name"]} {$user["Cognome"]} \n";
+      
+    } elseif($user["Genere"] == "Uomo"){
+        echo "Buongiorno Signore {$user["name"]} {$user["Cognome"]}\n";
+
+    } elseif($user["Genere"] == "NB") {
+        echo "Buongiorno {$user["name"]} {$user["Cognome"]}\n";
+
+    } else{
+        "Buongiorno";
+    }
+
+}
 
 
 
