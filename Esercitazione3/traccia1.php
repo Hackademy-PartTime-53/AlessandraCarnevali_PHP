5652df980cdf113409f6d249d36f0926560a387f
Alessandra Carnevali <alessandracarnevali@AirdiAlessandra.homenet.telecomitalia.it>
2025-03-05 07:56:10 -0800

    Esercizio 3 completo

--- Esercitazione3/traccia1.php
+++ Esercitazione3/traccia1.php
@@ -1,12 +1,55 @@
 <?php
 
-$Pippo = [4,7,9,8,2,];
+//Creazione Classe
+class Company{
 
-$result = array_map(function ($el){
-    return $el*2;
-}, $Pippo
+public $name;
+public $location;
+public $tot_employees;
+public $avg_salary;
+public static $counter = 0;
 
-);
+
+//Creazione costrutto
+public function __construct($nome, $luogo, $dipendenti, $salario){
+
+    $this->name = $nome;
+    $this->location = $luogo;
+    $this->tot_employees = $dipendenti;
+    $this->avg_salary = $salario;
+    //Invoco la funzione stampa info
+    $this-> stampaInfo();
+    //Metto il self per tenere conto delle istanze
+    self::$counter++;
+
+}
+
+//Creazione metodo di stampa frase
+public function stampaInfo(){
+    if($this->tot_employees >0){
+        echo " L'ufficio $this->name con sede in $this->location ha ben $this->tot_employees dipendenti ed uno stipendio di $this->avg_salary euro. \n ";
+    } else {
+        echo " L'ufficio $this->name con sede in $this->location non ha dipendenti. \n ";
+    }
+}
+
+//Creazione metodo collegato al self counter
+public static function howManyCompany(){
+    echo " Al momento ci sono " . self::$counter . " compagnie \n ";
+}
+
+}
 
 
-print_r ($result);
+
+//Creazione 5 Istanze
+Company::howManyCompany();
+$Company1 = new Company (" Aulab ", " Italia ", 50, 2000);
+$Company2 = new Company ("SunsetStudios", "Los Angeles", 300, 3000);
+$Company3 = new Company ("Columbia", "Georgia", 400, 4000);
+$Comoany4 = new Company ("Paramount", "Texas", 0, 3500);
+$Company5 = new Company ("Universal", "California", 3000, 4000);
+Company::howManyCompany();
+
+
+
